835ae8bc41c29bb9adc10f72013c0994640ea45d
Victor Lin <swissashley@hotmail.com>
2016-10-13 18:13:51 -0700

    Added String

--- lib/ch7_string.rb
+++ lib/ch7_string.rb
@@ -1,14 +1,27 @@
+# 7.1
+# Convert a number into string
 def int_to_str(num)
   is_negative = num < 0
-  out_str = is_negative ? "-" : ""
   in_num = num.abs
+  out_str = ""
   while in_num > 0
     out_str = ('0'.ord + in_num % 10).chr + out_str
     in_num /= 10
   end
+  out_str = "-" + out_str if is_negative
   out_str
 end
 
-def string_to_int(str)
-
+# 7.1
+# Convert a string to a number
+def str_to_int(str)
+  is_negative = true if str[0] == "-"
+  start = is_negative ? 1 : 0
+  num = 0
+  start.upto(str.length - 1) do |idx|
+    num = num * 10 + (str[idx, 1].ord - "0".ord)
+  end
+  return is_negative ? -num : num
 end
+
+#
--- spec/ch7_string_spec.rb
+++ spec/ch7_string_spec.rb
@@ -5,6 +5,15 @@ describe "String interview questions" do
     it "converts a number to string" do
       # p swap_even_odd(arr)
       expect(int_to_str(12345)).to eq("12345")
+      expect(int_to_str(-12345)).to eq("-12345")
+    end
+  end
+
+  describe "#str_to_int" do
+    it "converts a string to number" do
+      # p swap_even_odd(arr)
+      expect(str_to_int("12345")).to eq(12345)
+      expect(str_to_int("-12345")).to eq(-12345)
     end
   end
 
