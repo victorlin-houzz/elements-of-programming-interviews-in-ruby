06ca3d1e44d03769766bd5f60caa95e522bf70d6
Victor Lin <swissashley@hotmail.com>
2016-10-13 17:40:26 -0700

    finish Array

--- lib/ch6_array.rb
+++ lib/ch6_array.rb
@@ -8,7 +8,7 @@ def swap_even_odd(arr)
     if arr[even_idx] % 2 == 0
       even_idx += 1
     else
-      arr[even_idx], arr[odd_idx] = arr[odd_idx], arr[even_idx] 
+      arr[even_idx], arr[odd_idx] = arr[odd_idx], arr[even_idx]
       # arr[even_idx] ^= arr[odd_idx]
       # arr[odd_idx] ^= arr[even_idx]
       # arr[even_idx] ^= arr[odd_idx]
@@ -41,8 +41,125 @@ def dutch_flag_partition(arr, idx)
       arr[equal], arr[large_pivot] = arr[large_pivot], arr[equal]
       large_pivot -= 1
     end
-    p arr
+    # p arr
   end
 
   arr
+end
+
+# 6.6
+# Return the idx pair for the max profit for buying & selling a stock.
+# O(n) time and O(1) space.
+def stock_max_profit(arr)
+  max_profit = 0
+  min = arr[0]
+  min_idx = 0
+  max_idx = 0
+  1.upto(arr.length - 1) do |idx|
+    if arr[idx] < min
+      min_idx = idx
+      min = arr[idx]
+    end
+
+    if (arr[idx] - min) > max_profit
+      max_idx = idx
+      max_profit = arr[idx] - min
+    end
+  end
+  [min_idx, max_idx]
+end
+
+# 6.11
+# Random subset of an array with size n
+# O(n) time and O(1) space.
+def random_subset(arr, n)
+  sub_arr = arr.dup
+  return nil if n > sub_arr.length
+  0.upto(n) do |idx|
+    rand_idx = rand(sub_arr.length)
+    sub_arr[idx], sub_arr[rand_idx] = sub_arr[rand_idx], sub_arr[idx]
+  end
+  sub_arr[0...n]
+end
+
+#6.17
+# Return an array in spiral order for a 2D array
+# O(n^2) time and O(1) space.
+def matrix_spiral(arr)
+  shift = [[0,1], [1,0], [0,-1], [-1,0]]
+  dup_arr = deep_dup(arr)
+  p dup_arr
+  out_arr = []
+  idx = 0
+  x = 0
+  y = 0
+  dir = 0
+
+  while idx < dup_arr.length * dup_arr[0].length
+    out_arr << dup_arr[x][y]
+    next_x = x + shift[dir][0]
+    next_y = y + shift[dir][1]
+    dup_arr[x][y] = nil
+
+    if next_x < 0 || next_x >= dup_arr.length ||
+      next_y < 0 || next_y >= dup_arr[0].length || dup_arr[next_x][next_y] == nil
+      dir = (dir + 1) % 4
+    end
+
+    x += shift[dir][0]
+    y += shift[dir][1]
+    idx += 1
+  end
+  # p out_arr
+  out_arr
+end
+
+def deep_dup(arr)
+  return arr if arr.length <= 1
+  out_arr = []
+  arr.each do |el|
+    out_arr << (el.is_a?(Array) ? deep_dup(el) : el)
+  end
+  out_arr
+end
+
+# 6.18 Rotate an array clockwise or counter-clockwise
+# O(n^2) time and O(1) space.
+def rotate_matrix(arr, clockwise)
+  dup_arr = deep_dup(arr)
+  temp_arr = []
+  size = dup_arr.length - 1
+  0.upto(dup_arr.length / 2) do |i|
+    i.upto(dup_arr.length - 2 - i) do |j|
+      if clockwise
+        p "#{i}, #{j}"
+        # p "#{dup_arr[i][j]}, #{dup_arr[j][size - i]}, #{dup_arr[size - i][size - j]}, #{dup_arr[size - j][i]}"
+        dup_arr[i][j], dup_arr[j][size - i], dup_arr[size - i][size - j], dup_arr[size - j][i] =
+        dup_arr[size - j][i], dup_arr[i][j], dup_arr[j][size - i], dup_arr[size - i][size - j]
+        # p "#{dup_arr[i][j]}, #{dup_arr[j][size - i]}, #{dup_arr[size - i][size - j]}, #{dup_arr[size - j][i]}"
+
+      else
+        dup_arr[i][j], dup_arr[j][size - i], dup_arr[size - i][size - j], dup_arr[size - j][i] =
+        dup_arr[j][size - i], dup_arr[size - i][size - j], dup_arr[size - j][i], dup_arr[i][j]
+      end
+    end
+  end
+  # p dup_arr
+  dup_arr
+end
+
+# 6.19 Pascal's triangle
+# O(n^2) time and O(1) space.
+def pascal_triangle(n)
+  return [[]] if n < 0
+  return [[1]] if n == 1
+  return [[1],[1,1]] if n == 2
+  last_result = pascal_triangle(n - 1)
+  last_row = last_result.last
+  out_arr = [1]
+  0.upto(last_row.length - 2) do |idx|
+    out_arr << last_row[idx] + last_row[idx + 1]
+  end
+  out_arr << 1
+  last_result << out_arr
 end
--- spec/ch6_array_spec.rb
+++ spec/ch6_array_spec.rb
@@ -5,14 +5,63 @@ describe "Array interview questions" do
     it "swaps even number to the front of the array" do
       arr = [1,5,6,7,8,2,3]
       # p swap_even_odd(arr)
-      expect(swap_even_odd(arr)).to eq([2,8,6,5,3,1,7])
+      expect(swap_even_odd(arr)).to eq([2, 8, 6, 7, 5, 3, 1])
     end
   end
   describe "#dutch_flag_partition" do
-    it "swaps even number to the front of the array" do
+    it "partitions an array into 3 sections" do
       arr = [8,2,9,7,6,1,3]
-      # p swap_even_odd(arr)
-      expect(dutch_flag_partition(arr, 3)).to eq([2,8,6,5,3,1,7])
+
+      expect(dutch_flag_partition(arr, 3)).to eq([3, 2, 1, 6, 7, 9, 8])
+    end
+  end
+
+  describe "#stock_max_profit" do
+    it "calculates the maximum profit for a stock" do
+      arr = [15,5,10,20,5,65,0,30,15,75,20]
+
+      expect(stock_max_profit(arr)).to eq([6,9])
+    end
+  end
+
+  describe "#random_subset" do
+    it "returns a random subset of an array" do
+      arr = [1,2,3,4,5]
+
+      expect(random_subset(arr, 3)).not_to eq([1,2,3])
+    end
+  end
+
+  describe "#matrix_spiral" do
+    it "returns an array with spiral order of a 2D array" do
+      arr = [[1,2,3],[4,5,6],[7,8,9 ]]
+
+      expect(matrix_spiral(arr)).to eq([1,2,3,6,9,8,7,4,5])
+    end
+  end
+
+  describe "#rotate_matrix" do
+    it "returns an array with clockwise/counter-clockwise order of a 2D array" do
+      arr = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+      out_arr1 = [[13,9,5,1],[14,10,6,2],[15,11,7,3],[16,12,8,4]]
+      out_arr2 = [[4,8,12,16],[3,7,11,15],[2,6,10,14],[1,5,9,13]]
+
+      expect(rotate_matrix(arr, true)).to eq(out_arr1)
+      expect(rotate_matrix(arr, false)).to eq(out_arr2)
+    end
+  end
+  describe "#pascal_triangle" do
+    it "returns a pascal triangle array" do
+      arr2 = [[1], [1,1]]
+      arr3 = [[1], [1,1], [1,2,1]]
+      arr4 = [[1], [1,1], [1,2,1], [1,3,3,1]]
+      arr5 = [[1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1]]
+
+      expect(pascal_triangle(2)).to eq(arr2)
+      expect(pascal_triangle(3)).to eq(arr3)
+      expect(pascal_triangle(4)).to eq(arr4)
+      expect(pascal_triangle(5)).to eq(arr5)
+
     end
   end
 end
