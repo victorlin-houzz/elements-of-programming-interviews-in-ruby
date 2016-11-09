02c0529d0607df8f75966e8ac23428e649f4fe02
Victor Lin <swissashley@hotmail.com>
2016-11-09 10:33:33 -0800

    added search

--- lib/ch12_search.rb
+++ lib/ch12_search.rb
@@ -1,5 +1,6 @@
-# search a sorted array for first occurrence of k
+# 12.1 search a sorted array for first occurrence of k
 # arr = [0,4,5,5,5,7,7,9,10,11,12], k = 5, out = 2
+# Time: O(logn)
 def search_first_of_k(arr, k)
   result = nil
   left = 0
@@ -16,4 +17,34 @@ def search_first_of_k(arr, k)
     end
   end
   result
+end
+
+# 12.3 search the smallest element in a cyclically array
+# arr = [5,6,7,8,9,10,1,2], out = index 5
+
+def search_the_smallest(arr)
+  mid = nil
+  left = 0
+  right = arr.length - 1
+  while left < right
+    mid = left + (right - left) / 2
+    if arr[mid] > arr[right]
+      left = mid + 1
+    else
+      right = mid
+    end
+  end
+  left
+end
+
+# 12.4 find the largest x ,where x^2 < k, and (x + 1)^2 > k
+def find_closest_sq_root(k)
+  left = 0
+  right = k
+  while left <= right
+    mid = left + (right - left) / 2
+    right = mid - 1 if mid * mid > k
+    left = mid + 1 if mid * mid <= k
+  end
+  return left - 1
 end
