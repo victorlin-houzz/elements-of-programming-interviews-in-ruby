6d74b046a7518a3bc84e981b6847eea084b8a2a1
Victor Lin <swissashley@hotmail.com>
2016-11-10 10:19:56 -0800

    modified hash

--- lib/ch13_hash.rb
+++ lib/ch13_hash.rb
@@ -1,3 +1,21 @@
+# 13.1: take in a string and return whether any of it's permutations are palindromes
+# Time: O(n)
+def per_palindromes?(str)
+  hash = Hash.new{0}
+
+  # Find the frequency for each char
+  str.chars.each do |c|
+    hash[c] += 1
+  end
+
+  odd_count = 0
+  hash.each do |key, value|
+    odd_count += 1 unless value % 2 == 0
+    return false if odd_count > 1
+  end
+  true
+end
+
 # 13.4 LCA with hash
 # Time: O(D0 + D1), Space: O(D0 + D1)
 def hash_lca(node0, node1)
