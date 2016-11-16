943da9ab81fa90f6c0776a40409c1d158586ab2a
Victor Lin <swissashley@hotmail.com>
2016-11-16 14:22:16 -0800

    Generate Palindromic decompositions

--- lib/ch16_recursion.rb
+++ lib/ch16_recursion.rb
@@ -65,3 +65,38 @@ def build_balanced_parens(num_left_parens_needed, num_right_parens_needed, valid
   end
   result
 end
+
+# 16.7 Generate Palindromic decompositions
+# Time: O(n 2^n)
+def generate_palindromic_decompositions(input)
+  result = []
+  build_palindromic_compositions(input, 0, [], result)
+end
+
+def build_palindromic_compositions(input, start, partial_partition, result)
+  # Base case
+  if start == input.length
+    result << partial_partition.dup
+    return
+  end
+
+  # partition
+  ending = start + 1
+  while ending <= input.length
+    prefix = input[start...ending]
+    if is_palindrome?(prefix)
+      partial_partition << prefix
+      build_palindromic_compositions(input, ending, partial_partition, result)
+      partial_partition.pop
+    end
+  ending += 1
+  end
+  result
+end
+
+def is_palindrome?(str)
+  0.upto(str.length / 2).each do |idx|
+    return false unless str[idx, 1] == str[str.length - idx - 1, 1]
+  end
+  return true
+end
